4081b02abe33e1ed5668ea721ea1eb68a2b9858c
chemi <wbsldj7645@naver.com>
2022-08-06 21:36:39 +0900

    [CHORE] 초기 프로젝트 세팅

---
+++ SwiftUI_DevNote/Global/Supports/SwiftUI_DevNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DevNoteApp.swift
+//  SwiftUI_DevNote
+//
+//  Created by Mingwan Choi on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DevNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
